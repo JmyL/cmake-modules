71ffb00c9ab20a9e4a7d84b4c4c6a974959f6bcd
Sungsik <sungsik@ultihash.io>
2025-10-27 19:28:53 +0100

    Add lineinfo option for cuda

--- EnableCUDA.cmake
+++ EnableCUDA.cmake
@@ -13,3 +13,7 @@ set(CMAKE_CUDA_HOST_COMPILER
 )
 enable_language(CUDA) # Enables CUDA language support
 include_directories(${CUDAToolkit_INCLUDE_DIRS})
+
+if(CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo" OR CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -lineinfo")
+endif()

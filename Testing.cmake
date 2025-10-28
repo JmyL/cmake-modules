84a2bee9632ac4baedb4568e04bf69e24417f251
Sungsik <sungsik@ultihash.io>
2025-10-28 13:53:21 +0100

    Add NO_GTEST_MAIN option for add_test_suite

--- Testing.cmake
+++ Testing.cmake
@@ -18,13 +18,19 @@ include(Coverage)
 include(Memcheck)
 
 macro(add_test_suite target)
+    set(options NO_GTEST_MAIN)
+    cmake_parse_arguments(TEST_SUITE "${options}" "" "" ${ARGN})
+
     add_coverage(${target})
-    target_link_libraries(${target} PRIVATE gtest_main gmock)
+    if(TEST_SUITE_NO_GTEST_MAIN)
+        target_link_libraries(${target} PRIVATE gmock)
+    else()
+        target_link_libraries(${target} PRIVATE gtest_main gmock)
+    endif()
     gtest_discover_tests(
         ${target}
         EXTRA_ARGS --gtest_color=yes
         PROPERTIES ENVIRONMENT "ASAN_OPTIONS=color=always"
     )
-
     add_memcheck(${target})
 endmacro()

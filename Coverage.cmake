e73958de1d9185ca119522dc13fe7ccc7513c995
Sungsik Nam <jmyl@me.com>
2026-09-05 17:25:41 +0200

    Make coverage tooling compiler-aware

--- Coverage.cmake
+++ Coverage.cmake
@@ -29,13 +29,44 @@ function(add_coverage target)
         return()
     endif()
 
+    if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        find_program(LLVM_COV_PATH NAMES llvm-cov llvm-cov-18)
+        if(NOT LLVM_COV_PATH)
+            message(WARNING "llvm-cov not found! Coverage target will not be created.")
+            return()
+        endif()
+
+        set(LLVM_GCOV_PATH "${CMAKE_BINARY_DIR}/llvm-gcov")
+        file(WRITE "${LLVM_GCOV_PATH}"
+            "#!/bin/sh\nexec \"${LLVM_COV_PATH}\" gcov \"\\$@\"\n"
+        )
+        file(CHMOD "${LLVM_GCOV_PATH}" PERMISSIONS
+            OWNER_READ OWNER_WRITE OWNER_EXECUTE
+            GROUP_READ GROUP_EXECUTE
+            WORLD_READ WORLD_EXECUTE
+        )
+        set(LCOV_TOOL_ARGS
+            --gcov-tool "${LLVM_GCOV_PATH}"
+            --ignore-errors mismatch,version,inconsistent
+        )
+        set(GENHTML_TOOL_ARGS --ignore-errors inconsistent)
+    else()
+        find_program(GCOV_PATH gcov)
+        if(NOT GCOV_PATH)
+            message(WARNING "gcov not found! Coverage target will not be created.")
+            return()
+        endif()
+        set(LCOV_TOOL_ARGS --gcov-tool "${GCOV_PATH}")
+        set(GENHTML_TOOL_ARGS)
+    endif()
+
     add_custom_target(
         coverage-${target}
-        COMMAND ${LCOV_PATH} -d . --zerocounters
+        COMMAND ${LCOV_PATH} ${LCOV_TOOL_ARGS} -d . --zerocounters
         COMMAND $<TARGET_FILE:${target}>
-        COMMAND ${LCOV_PATH} -d . --capture -o coverage.info
-        COMMAND ${LCOV_PATH} -r coverage.info '/usr/include/*' -o filtered.info
-        COMMAND ${GENHTML_PATH} -o coverage-${target} filtered.info --legend
+        COMMAND ${LCOV_PATH} ${LCOV_TOOL_ARGS} -d . --capture -o coverage.info
+        COMMAND ${LCOV_PATH} ${LCOV_TOOL_ARGS} -r coverage.info '/usr/include/*' -o filtered.info
+        COMMAND ${GENHTML_PATH} ${GENHTML_TOOL_ARGS} -o coverage-${target} filtered.info --legend
         COMMAND rm -rf coverage.info filtered.info
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
